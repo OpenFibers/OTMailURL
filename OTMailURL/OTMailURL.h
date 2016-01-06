1b5f9ce39b23bde434a3126ac91e82bfa63c1ae9
OpenThread <openfibers@gmail.com>
2016-01-06 11:11:52 +0800

    added mail url class

---
+++ OTMailURL/OTMailURL.h
@@ -0,0 +1,13 @@
+//
+//  OTMailURL.h
+//  OTMailURLDemo
+//
+//  Created by openthread on 1/6/16.
+//  Copyright © 2016 openthread. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OTMailURL : NSObject
+
+@end
---
+++ OTMailURL/OTMailURL.m
@@ -0,0 +1,13 @@
+//
+//  OTMailURL.m
+//  OTMailURLDemo
+//
+//  Created by openthread on 1/6/16.
+//  Copyright © 2016 openthread. All rights reserved.
+//
+
+#import "OTMailURL.h"
+
+@implementation OTMailURL
+
+@end
