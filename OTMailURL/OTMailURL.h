b76101a964a60f196a909076599a4bc52f1084ad
OpenThread <openfibers@gmail.com>
2016-01-06 12:14:12 +0800

    added testcase

--- OTMailURL/OTMailURL.h
+++ OTMailURL/OTMailURL.h
@@ -10,4 +10,15 @@
 
 @interface OTMailURL : NSObject
 
++ (instancetype)URLWithString:(NSString *)URLString;
+
+- (instancetype)initWithString:(NSString *)URLString;
+
+@property (nonatomic, readonly) NSString *originalURLString;
+@property (nonatomic, strong) NSString *toMailAddress;
+@property (nonatomic, strong) NSString *ccMailAddress;
+@property (nonatomic, strong) NSString *bccMailAddress;
+@property (nonatomic, strong) NSString *subject;
+@property (nonatomic, strong) NSString *body;
+
 @end
--- OTMailURL/OTMailURL.m
+++ OTMailURL/OTMailURL.m
@@ -8,6 +8,49 @@
 
 #import "OTMailURL.h"
 
+@interface OTMailURL ()
+@end
+
 @implementation OTMailURL
+
++ (instancetype)URLWithString:(NSString *)URLString
+{
+    return [[self alloc] initWithString:URLString];
+}
+
+- (instancetype)initWithString:(NSString *)URLString
+{
+    NSString *schemeLowercase = [[[self class] schemeForURLString:URLString] lowercaseString];
+    if (![schemeLowercase isEqualToString:@"mailto"])
+    {
+        return nil;
+    }
+    
+    self = [super init];
+    if (self)
+    {
+        _originalURLString = URLString;
+        [self parseURL];
+    }
+    return self;
+}
+
+- (void)parseURL
+{
+    NSString *mailAndQueryString = [self.originalURLString substringFromIndex:@"mailto:".length];
+    
+}
+
++ (nullable NSString *)schemeForURLString:(NSString *)URLString
+{
+    NSString *const schemeSeparator = @":";
+    NSRange separatorRange = [URLString rangeOfString:schemeSeparator];
+    if (separatorRange.location != NSNotFound)
+    {
+        NSString *scheme = [URLString substringToIndex:separatorRange.location];
+        return scheme;
+    }
+    return nil;
+}
 
 @end
